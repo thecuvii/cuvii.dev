25ec3f97d5fdf6e23d2e6f24e431d9ca2ab555b1
Turbobot <turbobot@vercel.com>
2024-06-22 18:23:45 +0800

    feat(create-turbo): apply official-starter transform

--- components/mdx-renderer.tsx
+++
@@ -1,7 +0,0 @@
-import { useMDXComponent } from "next-contentlayer/hooks";
-
-export function MDXRenderer({ code }: { code: string }) {
-  const MDXContent = useMDXComponent(code);
-
-  return <MDXContent />;
-}
--- next.config.mjs
+++
@@ -1,5 +0,0 @@
-import { fileURLToPath } from "node:url";
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-export default jiti("./next.config.ts").default;
