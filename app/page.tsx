81c2e5b1a282599f72f0f9660c6d921601acadd8
Cuvii <i@cuvii.dev>
2025-04-05 13:15:38 +0800

    feat: enable dynamic I/O and PPR in Next.js configuration; update HomePage component for consistent styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <div className="h-screen grid place-items-center">
-      <h1 className="text-sm">Coming soon</h1>
+    <div className='h-screen grid place-items-center'>
+      <h1 className='text-sm'>Coming soon</h1>
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     useCache: true,
+    dynamicIO: true,
+    ppr: true,
   },
 }
 
