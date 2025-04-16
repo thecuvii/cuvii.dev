aa1a11313bc3c5059bbda7ae820124b21b4c7be5
Cuvii <i@cuvii.dev>
2025-04-16 16:16:05 +0800

    chore: add @egjs/react-infinitegrid and @zolplay/clsxm dependencies; update Gallery component to use MasonryInfiniteGrid for improved layout

--- features/gallery/gallery.tsx
+++ features/gallery/gallery.tsx
@@ -1,36 +1,42 @@
 'use client'
 
+import { MasonryInfiniteGrid } from '@egjs/react-infinitegrid'
+import { clsxm } from '@zolplay/clsxm'
 import Image from 'next/image'
 import { Masonry } from 'react-plock'
 import { generateCloudflareImageUrl } from './cf-image-loader'
 
 export function Gallery({ images }: { images: { url: string; aspectRatio: number; blurDataUrl: string }[] }) {
   return (
-    <main className='container mx-auto'>
-      <Masonry
-        items={images}
-        config={{
-          columns: [1, 2, 3, 4],
-          gap: [6, 6, 6, 6],
-          media: [640, 768, 1024, 1280],
-          useBalancedLayout: true,
-        }}
-        render={(image) => (
-          <div className='relative'>
+    <main className=''>
+      <MasonryInfiniteGrid>
+        {images.map((image) => (
+          <div className='relative' key={image.url}>
+            <Image
+              src={image.blurDataUrl}
+              alt=''
+              className={clsxm(
+                'select-none w-full h-auto',
+                image.aspectRatio && `aspect-[${image.aspectRatio}]`,
+                'min-w-[200px] sm:min-w-[300px] md:min-w-[400px] lg:min-w-[500px]',
+              )}
+              width={0}
+              height={0}
+            />
+
             <Image
               loader={generateCloudflareImageUrl}
               src={image.url}
               alt=''
-              className='select-none w-full h-auto'
+              loading='lazy'
+              className='absolute inset-0 select-none w-full h-auto'
               sizes='(min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw'
-              placeholder={image.blurDataUrl ? 'blur' : 'empty'}
-              blurDataURL={image.blurDataUrl}
               width={0}
               height={0}
             />
           </div>
-        )}
-      />
+        ))}
+      </MasonryInfiniteGrid>
     </main>
   )
 }
