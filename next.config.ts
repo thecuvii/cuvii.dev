974107ab791566bbba93005ea4f47a3a2c28bf5d
Cuvii <i@cuvii.dev>
2025-04-24 18:15:32 +0800

    chore: update Cache-Control max-age and s-maxage values in next.config.ts for optimized caching strategy

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=300, s-maxage=3000, stale-while-revalidate=14400, stale-if-error=14400',
+            value: 'public, max-age=0, s-maxage=604800, stale-while-revalidate=600, stale-if-error=86400',
           },
         ],
       },
