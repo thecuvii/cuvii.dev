8ebd0ac8d228a302027ba8c8f595174bf01c4e10
Cuvii <i@cuvii.dev>
2025-04-24 19:19:12 +0800

    chore: add SOURCE_COMMIT environment variable to Dockerfile for build tracking

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <div className='h-screen grid place-items-center'>
       <h1 className='text-sm'>Coming soon</h1>
+      <p className='text-xs'>{process.env.SOURCE_COMMIT}</p>
     </div>
   )
 }
