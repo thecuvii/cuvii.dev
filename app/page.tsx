a308b425daa3f9098cf312f9ced25a21f18e4f39
Cuvii <i@cuvii.dev>
2025-04-24 19:24:23 +0800

    fix: wrap content in a div for better structure in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function HomePage() {
   return (
     <div className='h-screen grid place-items-center'>
-      <h1 className='text-sm'>Coming soon</h1>
-      <p className='text-xs'>{process.env.SOURCE_COMMIT}</p>
+      <div>
+        <h1 className='text-sm'>Coming soon</h1>
+        <p className='text-xs'>{process.env.SOURCE_COMMIT}</p>
+      </div>
     </div>
   )
 }
