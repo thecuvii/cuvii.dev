486f59ce3c1478f2c32df188f3a1a0b57a433aa9
Cuvii <i@cuvii.dev>
2025-05-26 20:04:51 +0800

    chore: update next.config.ts to comment out experimental features and downgrade next version in package.json and pnpm-lock.yaml

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    useCache: true,
-    dynamicIO: true,
-    ppr: true,
+    // useCache: true,
+    // dynamicIO: true,
+    // ppr: true,
   },
   output: 'standalone',
   images: {
@@ -17,15 +17,15 @@ const nextConfig: NextConfig = {
   },
   async headers() {
     return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, s-maxage=604800, stale-while-revalidate=600, stale-if-error=86400',
-          },
-        ],
-      },
+      // {
+      //   source: '/',
+      //   headers: [
+      //     {
+      //       key: 'Cache-Control',
+      //       value: 'public, max-age=0, s-maxage=604800, stale-while-revalidate=600, stale-if-error=86400',
+      //     },
+      //   ],
+      // },
     ]
   },
 }
