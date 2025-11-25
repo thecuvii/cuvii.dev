f7da210020c5e4153128bea53995e73b1c188262
Cuvii <i@cuvii.dev>
2025-11-25 17:19:10 +0800

    Update favicon, modify global styles, and enhance layout for personal website; add manifest and Open Graph image

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: `Cuvii's site`,
+    short_name: `Cuvii's site`,
+    description: 'Personal website of Cuvii',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fafafa',
+    theme_color: '#fafafa',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,37 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black'>
-        <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={100} height={20} priority />
-        <div className='flex flex-col items-center gap-6 text-center sm:items-start sm:text-left'>
-          <h1 className='max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50'>
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className='max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400'>
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              className='font-medium text-zinc-950 dark:text-zinc-50'
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              className='font-medium text-zinc-950 dark:text-zinc-50'
-            >
-              Learning
-            </a>{' '}
-            center.
-          </p>
-        </div>
-        <div className='flex flex-col gap-4 text-base font-medium sm:flex-row'>
-          <a
-            className='bg-foreground text-background flex h-12 w-full items-center justify-center gap-2 rounded-full px-5 transition-colors hover:bg-[#383838] md:w-[158px] dark:hover:bg-[#ccc]'
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image className='dark:invert' src='/vercel.svg' alt='Vercel logomark' width={16} height={16} />
-            Deploy Now
-          </a>
-          <a
-            className='flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]'
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Documentation
-          </a>
-        </div>
+    <div className='flex min-h-screen flex-col justify-end font-bold tracking-tight'>
+      <main className='pb-12 pl-24'>
+        <section>
+          <h1 className='text-9xl font-black tracking-tighter'>Cuvii.</h1>
+          <p>Design Enginner. </p>
+          <p>Photographer. </p>
+        </section>
+
+        <section className='mt-12 text-xs font-bold tracking-normal'>
+          <ul>
+            <li>
+              <a className='underline-offset-4 hover:underline' href='mailto:yo@cuvii.dev'>
+                yo@cuvii.dev
+              </a>
+            </li>
+            <li>
+              <a className='hover:underline' href='https://x.com/thecuvii'>
+                x.com/thecuvii
+              </a>
+            </li>
+            <li>
+              <a className='hover:underline' href='https://github.com/thecuvii'>
+                github.com/thecuvii
+              </a>
+            </li>
+            <li>
+              <a className='hover:underline' href='https://unsplash.com/@cuvii'>
+                unsplash.com/@cuvii
+              </a>
+            </li>
+          </ul>
+        </section>
       </main>
     </div>
   )
