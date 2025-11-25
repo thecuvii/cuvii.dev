f2f4e16c716af5f690eefaebd6c670ac771d82a6
Cuvii <i@cuvii.dev>
2025-11-25 18:00:04 +0800

    Add Umami analytics script to RootLayout for tracking website usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { IBM_Plex_Mono, Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({
@@ -29,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <Script defer src='https://umami.cuvii.dev/script.js' data-website-id='c479f499-a6dd-4027-98b7-758b5fa9c704' />
       <body className={`${inter.variable} ${ibmPlexMono.variable} antialiased`}>{children}</body>
     </html>
   )
