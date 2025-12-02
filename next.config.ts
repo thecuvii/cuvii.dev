06a4a93f038635d359aef9b3fbf06c759afd71fa
Cuvii <i@cuvii.dev>
2025-12-02 10:13:25 +0800

    Update Next.js configuration to enable standalone output for improved deployment options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
